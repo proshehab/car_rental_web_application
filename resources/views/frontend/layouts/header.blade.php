b4acdcdeb031cd9fbc3424c021ad9248b707238a
proshehab <shehab.shakib@gmail.com>
2026-02-13 15:50:07 +0600

    Refactor home page layout and enhance header navigation
    - Improved indentation and formatting in home.blade.php for better readability.
    - Added a "Pick-up time" field in the car rental form on the home page.
    - Updated the services section to include a "Whole City Tour" service.
    - Corrected spelling errors in the "Featured Vehicles" section.
    - Enhanced the header navigation by adding a "Sign Up" button and improving the overall structure.

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,20 +1,30 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-      <a class="navbar-brand" href="{{url('/')}}">Car<span>Book</span></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span> Menu
-      </button>
+  <div class="container">
+    <a class="navbar-brand" href="{{url('/')}}">Car<span>Book</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="oi oi-menu"></span> Menu
+    </button>
 
-      <div class="collapse navbar-collapse" id="ftco-nav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active"><a href="{{url('/')}}" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="{{route('aboutPage')}}" class="nav-link">About</a></li>
-          <li class="nav-item"><a href="{{route('carPage')}}" class="nav-link">Cars</a></li>
-          <li class="nav-item"><a href="{{route('contactPage')}}" class="nav-link">Contact</a></li>
-          
-        </ul>
-        <a href="{{route('account.login')}}" class="btn btn-primary py-2 mr-1">Login</a> <a href="{{route('accoutnRegister')}}" class="btn btn-secondary py-2 ml-1">Sing Up</a></p>
-      </div>
+    <div class="collapse navbar-collapse" id="ftco-nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active"><a href="{{url('/')}}" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="{{route('aboutPage')}}" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="{{route('carPage')}}" class="nav-link">Cars</a></li>
+        <li class="nav-item"><a href="{{route('contactPage')}}" class="nav-link">Contact</a></li>
+        {{-- <li class="nav-item">
+          <a href="#" class="nav-link">
+            Cart
+            <span class="badge badge-light">
+              {{ session()->has('cart') ? count(session('cart')) : 0 }}
+            </span>
+          </a>
+        </li> --}}
+
+      </ul>
+      <a href="{{route('account.login')}}" class="btn btn-primary py-2 mr-1">Login</a> <a
+        href="{{route('accoutnRegister')}}" class="btn btn-secondary py-2 ml-1">Sing Up</a></p>
     </div>
-  </nav>
+  </div>
+</nav>
 <!-- END nav -->
